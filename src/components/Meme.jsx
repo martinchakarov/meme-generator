7d61b67c1b734534088d2536eeeead8131a81c27
martin_chakarov <martin.d.chakarov@gmail.com>
2022-11-05 02:09:31 +0200

    Add dynamic top and bottom text rendering

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,25 +4,54 @@ import memeData from "../data/memeData";
 export default function Meme() {
 
     function getRandomMemeImg(memeArray) {
-        return memeArray[Math.floor(Math.random() * memeArray.length)].url;
+        const index = Math.floor(Math.random() * memeArray.length)
+        return memeArray[index].url;
     }
 
-    const [memeImg, setMemeImg] = React.useState('');
+    
+    const [formData, setFormData] = React.useState({
+        topText: '', 
+        bottomText: '', 
+        randomImage: getRandomMemeImg(memeData.data.memes)
+    });
+
 
     function handleClick() {
-        setMemeImg(getRandomMemeImg(memeData.data.memes))
+        const newImg = getRandomMemeImg(memeData.data.memes);
+
+        setFormData((oldMemeData) => {
+            return {
+                ...oldMemeData,
+                randomImage: newImg
+            }
+        })
+    }
+
+    function handleInput(event) {
+        const {name, value} = event.target;
+
+        setFormData((oldFormData) => {
+            return {
+                ...oldFormData,
+                [name]: value
+            }
+        })
     }
 
     return (
         <div className="meme">
             <div className="form">
                <div className="input">
-                    <input type="text" placeholder="Top text"/>
-                    <input type="text" placeholder="Bottom text"/>
+                    <input type="text" name="topText" placeholder="Top text" onChange={handleInput}/>
+                    <input type="text" name="bottomText" placeholder="Bottom text" onChange={handleInput}/>
                </div>
                <button onClick={handleClick} className="generate-btn">Get a new meme image 🖼</button>
             </div>
-            {memeImg ? <img src={memeImg} className="memeImg"/> : null}
+            <div className="meme-wrapper">
+                <img src={formData.randomImage} className="memeImg"/>
+                <h2 className="meme-text top">{formData.topText}</h2>
+                <h2 className="meme-text bottom">{formData.bottomText}</h2>
+            </div>
         </div>
     )
 }
